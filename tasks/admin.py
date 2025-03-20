3982091ef43219f53499b7060c2394962c2db0a7
fredcounter <166711139+fredcounter@users.noreply.github.com>
2025-03-21 06:21:00 +0800

    Add files via upload

---
+++ tasks/admin.py
@@ -0,0 +1,10 @@
+#FREDERICK BEJERANO
+from django.contrib import admin
+from .models import Task
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'due_date', 'status')
+    list_filter = ('status', 'due_date')
+    search_fields = ('title', 'description')
+    ordering = ('due_date',)
